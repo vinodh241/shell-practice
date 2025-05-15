8df477ddbf03c0d3495cbabf1f05a525da793d0a
vinodh241 <a.vinodh9072@gmail.com>
2025-05-15 22:04:46 +0530

    shell-practie-2

---
+++ 03-varaibles.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1: : Hey Musk, How are you?"
+echo "$PERSON2:: Hello Trump, I am fine, how are you doing"
+echo "$PERSON1:: I am fine too, whats up"
+echo "$PERSON2:: Nothing, just going to marsh now, will you join?"
+echo "$PERSON1:: NO NO , you can  go, next time will join
+echo "$PERSON2:: Thank you
