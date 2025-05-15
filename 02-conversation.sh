d942a9c54d22f657965546569adcf96a09a8d4a1
vinodh241 <a.vinodh9072@gmail.com>
2025-05-15 21:58:02 +0530

    shell-practie-2

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+# !bin/bash
+
+echo "Trump: Hey musk how are you?"
+echo "Musk: i'm good, whats about you?"
+echo "Trump: I am also good, you know i stopped war between IND and PAK"
+echo "Musk: oh is it, i know whats hppened"
+echo "Trump: Thats great"
