f6b9df37a20827620248ad8605e6b00b087ee663
vinodh241 <a.vinodh9072@gmail.com>
2025-05-15 22:14:16 +0530

    shell-practie-4

---
+++ 04-commandline-args.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine, how are you doing"
+echo "$PERSON1:: I am fine too, whats up"
+echo "$PERSON2:: Nothing, just going to marsh now, will you join?"
+echo "$PERSON1:: NO NO , you can  go, next time will join
+echo "$PERSON2:: Thank you
