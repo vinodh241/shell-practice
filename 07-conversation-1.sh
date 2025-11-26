6d7aa4a0aab20de5dc3bcea783179e0ab4e58191
vinodh241 <a.vinodh9072@gmail.com>
2025-11-26 17:56:21 +0530

    Revise

---
+++ 07-conversation-1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Script Name: 07-conversation-1.sh
+
+echo "Hello Vinodh"
+Person1="Vinodh"
+Person2="AB"
+
+echo "$Person1: Hi AB How are you?"
+echo "$Person2: Hi Vinodh I am good. How about you?"
+echo "$Person1: I am doing well too. Thanks for asking!"
+echo "$Person2: What have you been up to lately?"
+echo "$Person1: Just working on some Bash scripting projects."
+echo "$Person2: That sounds interesting! Bash scripting is quite powerful."
+echo "$Person1: Yes, it is! I am learning a lot."
+echo "$Person2: Keep it up! Practice makes perfect."
+echo "$Person1: Thanks AB! I will."    
+echo "$Person2: No problem, Vinodh. Talk to you later!"           
+
+# End of script
