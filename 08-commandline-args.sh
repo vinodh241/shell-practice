a5b6b839939b015fa2f0ed36f895849ab2a5d466
vinodh241 <a.vinodh9072@gmail.com>
2025-11-26 18:06:31 +0530

    Revise

---
+++ 08-commandline-args.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#command line arguments example
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hey $Person2, How are you?"
+echo "$Person2: Hello $Person1, I am fine, how are you doing?"
+echo "$Person1: I am fine too, what's up?"
+echo "$Person2: Nothing, just going to the marsh now, will you join?"
+echo "$Person1: NO NO, you can go, next time I will join."
+echo "$Person2: Thank you!"
+
+    
+# End of script
