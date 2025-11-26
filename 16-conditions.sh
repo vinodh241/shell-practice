b4ef98f5363ad457232ee729fe6e8f2f7493bb24
vinodh241 <a.vinodh9072@gmail.com>
2025-11-26 21:54:17 +0530

    shell-Scripting-Revise

---
+++ 16-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u )
+
+if [$USERID -ne 0 ]
+ then
+   echo "ERROR:: Please run this script with root access"
+  
+   else 
+        echo "Success:: you are running with root access"
+
+fi
+
+dnf install httpd -y 
+
