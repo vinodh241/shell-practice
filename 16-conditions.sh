cf340b6c2cccd6627b523c268309ab2522ab8ef2
vinodh241 <a.vinodh9072@gmail.com>
2025-11-26 21:59:58 +0530

    shell-Scripting-Revise

--- 16-conditions.sh
+++ 16-conditions.sh
@@ -5,6 +5,7 @@ USERID=$(id -u )
 if [ $USERID -ne 0 ]
  then
    echo "ERROR:: Please run this script with root access"
+   exit 1 # we can give other than zero upto 127 
   
    else 
         echo "Success:: you are running with root access"
