d8716512d48a14698f88bcf162421c41cce2c0f4
vinodh241 <a.vinodh9072@gmail.com>
2025-11-27 12:35:10 +0530

    shell-Scripting-Revise

--- 19-logs.sh
+++ 19-logs.sh
@@ -7,9 +7,17 @@ Y="\e[33m"
 N="\e[0m"
 
 LOG_FOLDER="/var/log/myscript"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
 echo -e "script started exuction time : $(date)" &>> $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: pleae run this script with root access $N" &>> $LOG_FILE
+    exit 1 # we can give other than zero upto 127
+else
+    echo -e "$G Success:: you are running with root access $N" &>> $LOG_FILE
+fi
 
