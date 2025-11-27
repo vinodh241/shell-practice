0250d0ab77d5ae1fd1732f2f39eb2f779fae44c8
vinodh241 <a.vinodh9072@gmail.com>
2025-11-27 11:31:36 +0530

    shell-Scripting-Revise

--- 18-colors.sh
+++ 18-colors.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo -e "\e[31m Hello vinodh \e[0m"
+echo -e "\e[32m Hello vinodh \e[0m"
 
-echo "Good morning"
+
+USERID=$(id -u)
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+B=\e[34m
+W=\e[37m
+N=\e[0m
+
+if [ $USEDID -ne 0]
+then
+    echo -e "$R ERROR:: pleae run this script with root access $N"
+    exit 1 # we can give other than zero upto 127
+else
+    echo -e "$G Success:: you are running with root access $N"
+fi
